1daeffa74ef210afedfbd1a253058713c69ed037
Dmitry Markov <forall@stylive.com>
2019-07-27 16:43:16 +0200

    feat(graphql): add root mutations

--- src/server/services/graphql/schema.js
+++ src/server/services/graphql/schema.js
@@ -10,12 +10,29 @@ const typeDefinitions = `
     username: String
   }
   
+  input PostInput {
+    text: String!
+  }
+  
+  input UserInput {
+    username: String!
+    avatar: String!
+  }
+  
   type RootQuery {
     posts: [Post]
   }
   
+  type RootMutation {
+    addPost (
+      post: PostInput!
+      user: UserInput!
+    ): Post
+  }
+  
   schema {
     query: RootQuery
+    mutation: RootMutation
   }
 `
 
