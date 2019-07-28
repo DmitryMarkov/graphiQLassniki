4cf5c13e36a6d6b7ad4dc2d0fb7c77bf312da866
Dmitry Markov <forall@stylive.com>
2019-07-28 10:51:14 +0200

    feat(db): update seeds

--- src/server/seeders/20190728083652-fake-users.js
+++ src/server/seeders/20190728083652-fake-users.js
@@ -3,15 +3,17 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
-      'Posts',
+      'Users',
       [
         {
-          text: 'Lorem ipsum 1',
+          avatar: '/uploads/avatar1.png',
+          username: 'TestUser',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          text: 'Lorem ipsum 2',
+          avatar: '/uploads/avatar2.png',
+          username: 'TestUser2',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -21,6 +23,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Posts', null, {})
+    return queryInterface.bulkDelete('Users', null, {})
   },
 }
