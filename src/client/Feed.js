e0829490469d86def760d5c6046b1a1d0e7ac962
Dmitry Markov <forall@stylive.com>
2019-07-28 16:52:09 +0200

    feat(apollo): fetching data with graphql hoc

---
+++ src/client/Feed.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+const GET_POSTS = gql`
+  {
+    posts {
+      id
+      text
+      user {
+        avatar
+        username
+      }
+    }
+  }
+`
+
+class Feed extends Component {
+  state = {
+    postContent: '',
+  }
+  handlePostContentChange = event => {
+    this.setState({ postContent: event.target.value })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    const newPost = {
+      id: this.state.posts.length + 1,
+      text: this.state.postContent,
+      user: {
+        avatar: '/uploads/avatar3.png',
+        username: 'Fake User',
+      },
+    }
+    this.setState(prevState => ({
+      posts: [newPost, ...prevState.posts],
+      postContent: '',
+    }))
+  }
+
+  render() {
+    const { posts, loading, error } = this.props
+    const { postContent } = this.state
+
+    if (loading) {
+      return 'loading...'
+    }
+
+    if (error) {
+      return error.message
+    }
+
+    return (
+      <div className="container">
+        <div className="postForm">
+          <form onSubmit={this.handleSubmit}>
+            <textarea
+              value={postContent}
+              onChange={this.handlePostContentChange}
+              placeholder="Write your custom post!"
+            />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="feed">
+          {posts.map((post, i) => (
+            <div key={post.id} className="post">
+              <div className="header">
+                <img src={post.user.avatar} />
+                <h2>{post.user.username}</h2>
+              </div>
+              <p className="content">{post.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default graphql(GET_POSTS, {
+  props: ({ data: { loading, error, posts } }) => ({
+    loading,
+    error,
+    posts,
+  }),
+})(Feed)
